8c4e15e035ff5e7dd5d221b0291e2d76e73a07b9
Ian <holmesian17@gmail.com>
2021-02-09 13:04:00 -0700

    Add files via upload

--- searchDataPython.py
+++ searchDataPython.py
@@ -3,33 +3,36 @@ import hashlib
 import json
 from spellchecker import SpellChecker
 import urllib3
+from bs4 import BeautifulSoup
+import requests
 
+# CSV header names
 fieldnames = ['Search','Corrected','Timestamp','IP',
               'Search Function','Category']
 
-searchFileIn = open('rawDataSampleWithCats.csv', "rt")
-reader = csv.DictReader(searchFileIn)
+search_file_in = open('rawDataSampleWithCats.csv', "rt")
+reader = csv.DictReader(search_file_in)
 
-searchFileOut = open('dataSampleFormatted.csv', "wt")
-writer = csv.DictWriter(searchFileOut,
+search_file_out = open('dataSampleFormatted.csv', "wt")
+writer = csv.DictWriter(search_file_out,
                         fieldnames=(fieldnames))
 writer.writeheader()
 # categoryDict = {}
 # need to save these to an external file to pull from
 
 with open("categoryDict.json", "r") as config_file:
-    categoryDict = json.load(config_file)
+    category_dict = json.load(config_file)
 
-print(categoryDict)
+print(category_dict)
 
 spell = SpellChecker(distance=1)
 
 for row in reader:
     #hashing ip address
-    ipAddress = row['IP'] #3
+    ip_address = row['IP'] #3
 
     #print(ipAddress)
-    hash_object = hashlib.sha1(ipAddress.encode('utf-8')).hexdigest()
+    hash_object = hashlib.sha1(ip_address.encode('utf-8')).hexdigest()
     #print(hash_object)
     row['IP'] = hash_object
     
@@ -38,30 +41,34 @@ for row in reader:
     category = row['Category'] #5
     
     term = term.lower()
-    # there's an easier way to add to dict in the readthedocs for pyspellchecker
-    correctedText = spell.correction(term)
     
-    row['Corrected'] = correctedText #1
-    '''
-    # this drastically slows things down - maybe not show off at first but talk out
-    # with Tova
+    # there's an easier way to add to dict in the readthedocs for pyspellchecker
+    corrected_text = spell.correction(term)
+    
+    row['Corrected'] = corrected_text #1
+
+    # open library subjects for keyword result
+    # 5 categories are the 5 most common subjects for the keyword search
+    # add to dictionary and automatically add categories if that term is searched again
+    
+    # ALT: harvard library api - api.harvard.edu/v2/items?q=peanuts - returns XML
+    # harvard library api - api.harvard.edu/v2/items.json?q=peanuts - returns JSON
+    # 5 most common topics in the XML for the five
+    # add to dictionary and automatically add categories if that term is searched again
+    # wiki.harvard.edu/confluence/display/LibraryStaffDoc/LibraryCloud+APIs
+    
     http = urllib3.PoolManager()
-    # make the API search for whatever the search term was
-    # this API also seems to have subjects and other fun stuff
-    # could be useful in the categories thing
-    # for example, most common subject of results = the category???
-    # only works for books currently, how to handle dvds, etc.?
-    term = term.split()
-    term = '+'.join(term)
-    apiURL = str("http://openlibrary.org/search.json?title=" + term)
-    # for multiple words it needs to split them and add a + between
-    books = http.request('GET', apiURL)
-    bookDict = json.loads(books.data.decode('UTF-8'))
-    '''
+    API_URL = str("https://api.lib.harvard.edu/v2/items?q=" + term)
+    topicJSON = http.request('GET', API_URL)
+    soup = BeautifulSoup(topicJSON, 'html.parser')
+    site_json=json.loads(soup.text)
+
+    print([d.get('topic') for d in site_json['subject'] if d.get('topic')])
+    
     writer.writerow(row)
     
     print(term)
-    print(correctedText)
+    print(corrected_text)
   
     
 '''
